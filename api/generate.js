6b6dd98034e0decacca16ba1f8d8b0dabb4bf618
stepanovayasmin55-eng <stepanovayasmin55@gmail.com>
2026-08-19 14:21:48 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -43,7 +43,7 @@ module.exports = async (req, res) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'llama3-70b-8192',
+        model: 'llama-3.1-8b-instant',
         messages: [
           {
             role: 'user',
